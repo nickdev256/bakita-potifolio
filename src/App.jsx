22a749fac82c82c95e747b2441b50436965597f5
Nicholas Ssentongo <nickclas256@gmail.com>
2026-06-23 17:48:14 +0300

    Remove node_modules from repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <div>
-      <h1>Bakita Law Portfolio</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
