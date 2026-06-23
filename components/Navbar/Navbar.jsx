41e59de41de502fb4ed87df8495f382a697b6df9
Nicholas Ssentongo <nickclas256@gmail.com>
2026-06-23 15:17:25 +0300

    Fix About.css

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./Navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className="navbar">
-
+      
       <div className="logo-section">
-
         <img
           src="/law-logo.png"
           alt="Bakita Lydia Logo"
@@ -20,36 +27,52 @@ function Navbar() {
             LAW STUDENT • RESEARCHER • ADVOCATE
           </p>
         </div>
-
       </div>
 
-      <nav>
+      {/* Mobile Menu Button */}
+      <div
+        className="menu-toggle"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      {/* Navigation */}
+      <nav className={menuOpen ? "active" : ""}>
         <ul className="nav-links">
 
-          <li><Link to="/">Home</Link></li>
-
-          <li><Link to="/about">About</Link></li>
+          <li>
+            <Link to="/" onClick={closeMenu}>
+              Home
+            </Link>
+          </li>
 
           <li>
-            <Link to="/academic">
+            <Link to="/about" onClick={closeMenu}>
+              About
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/academic" onClick={closeMenu}>
               Academic Journey
             </Link>
           </li>
 
           <li>
-            <Link to="/publications">
+            <Link to="/publications" onClick={closeMenu}>
               Publications
             </Link>
           </li>
 
           <li>
-            <Link to="/moot-court">
+            <Link to="/moot-court" onClick={closeMenu}>
               Moot Court
             </Link>
           </li>
 
           <li>
-            <Link to="/contact">
+            <Link to="/contact" onClick={closeMenu}>
               Contact
             </Link>
           </li>
